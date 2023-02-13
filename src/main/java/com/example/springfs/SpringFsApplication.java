8bc64a133e48b8f379809fc3a357b3dd51726caa
Lukasz <ryczekl21@gmail.com>
2023-02-13 23:51:30 +0100

    initial commit

---
+++ src/main/java/com/example/springfs/SpringFsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springfs/SpringFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
