7f7bc304de93097c94694bc2e44addf3ea15b3a6
Lukasz <ryczekl21@gmail.com>
2023-02-16 01:54:26 +0100

    added registration and login backend

---
+++ src/main/java/com/example/springfs/service/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.example.springfs.service;
+
+import com.example.springfs.config.AuthenticationRequest;
+import com.example.springfs.config.AuthenticationResponse;
+import com.example.springfs.config.RegisterRequest;
+import com.example.springfs.enums.Role;
+import com.example.springfs.model.User;
+import com.example.springfs.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var token = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword())
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(()->new UsernameNotFoundException("User not found"));
+        var token = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+}
